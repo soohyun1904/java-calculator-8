b91d07f7194d7082b945ecd5a15a48dec0a3c284
SooHyeon Lee <dalsu4594@naver.com>
2025-10-19 14:19:41 +0900

    feat: 피연산자 역할을 하는 기능 구현 단일 숫자를 표현하고 검증하는 Operand 클래스, 상수 추가

--- src/main/java/calculator/constant/Message.java
+++ src/main/java/calculator/constant/Message.java
@@ -11,4 +11,6 @@ public class Message {
     public static final String ERROR_EMPTY_DELIMITER = "[ERROR] 구분자가 비어있습니다.";
     public static final String ERROR_DELIMITER_LENGTH = "[ERROR] 구분자는 1글자만 가능합니다.";
     public static final String ERROR_DELIMITER_NUMERIC = "[ERROR] 구분자에 숫자를 사용할 수 없습니다.";
+    public static final String NEGATIVE_NOT_ALLOWED = "[ERROR] 음수는 허용되지 않습니다.";
+    public static final String NUMBER_PARSE_ERROR = "[ERROR] 숫자로 변환할 수 없습니다.";
  }
---
+++ src/main/java/calculator/domain/Operand.java
@@ -0,0 +1,34 @@
+package calculator.domain;
+
+import static calculator.constant.Message.*;
+
+public class Operand {
+    private final int value;
+
+    private Operand(int value) {
+        validate(value);
+        this.value = value;
+    }
+
+    public static Operand from(String numbersPart){
+        if(numbersPart.isEmpty()){
+            return new Operand(0);
+        }
+        try{
+            int number = Integer.parseInt(numbersPart.trim());
+            return new Operand(number);
+        }catch (NumberFormatException e){
+            throw new IllegalArgumentException(NUMBER_PARSE_ERROR);
+        }
+    }
+
+    private void validate(int value) {
+        if (value < 0) {
+            throw new IllegalArgumentException(NEGATIVE_NOT_ALLOWED);
+        }
+    }
+
+    public int getValue(){
+        return value;
+    }
+}
