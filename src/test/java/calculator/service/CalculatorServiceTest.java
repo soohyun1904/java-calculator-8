836dd35832e0d17cd6c62973023b91783a7f82ac
SooHyeon Lee <dalsu4594@naver.com>
2025-10-20 20:53:43 +0900

    test: Calculator 테스트 수정 Caculator 객체를 Caculator과 CaculatorService로 나눠 문제가 생김

--- src/test/java/calculator/service/CalculatorServiceTest.java
+++ src/test/java/calculator/service/CalculatorServiceTest.java
@@ -1,18 +1,49 @@
-package calculator.domain;
+package calculator.service;
 
+import calculator.config.AppConfig;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class CalculatorTest {
+public class CalculatorServiceTest {
+    private CalculatorService calculatorService;
+
+    @BeforeEach
+    void set(){
+        AppConfig appConfig = new AppConfig();
+        calculatorService = appConfig.calculatorService();
+    }
 
     @Test
     @DisplayName("기본 구분자로 분리하여 숫자들의 합을 구한다.")
     void calculateSumWithDefaultDelimiter(){
         String input = "1:4,5:6";
-        Calculator cal =
-        int result = cal.sum();
+        int result = calculatorService.calculate(input);
         assertThat(result).isEqualTo(16);
+    }
+
+    @Test
+    @DisplayName("커스텀 구분자로 분리하여 숫자들의 합을 구한다.")
+    void calculateSumWithCustomDelimiter(){
+        String input = "///\\n1/2/3/4/5";
+        int result = calculatorService.calculate(input);
+        assertThat(result).isEqualTo(15);
+    }
+
+    @Test
+    @DisplayName("빈 문자열은 그 합은 0이다")
+    void calculateEmptyString(){
+        String input = "";
+        int result = calculatorService.calculate(input);
+        assertThat(result).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("구분자 사이가 비면 빈 자리는 0으로 계산된다.")
+    void emptyBetweenDelimitersIsZero(){
+        String input = "1::3";
+        int result = calculatorService.calculate(input);
+        assertThat(result).isEqualTo(4);
     }
 }
