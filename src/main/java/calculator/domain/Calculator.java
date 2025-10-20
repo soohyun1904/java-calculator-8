2b7502298c84e1485b45606f0f338b37fdbcaec1
SooHyeon Lee <dalsu4594@naver.com>
2025-10-20 19:53:56 +0900

    refactor: Operands에서 sum 삭제후 iterator 추가 Operands 객체에서 계산하는 책임을 가지고 있었기 때문에 iterator을 만들어 Calculator객체에게 책임을 넘김

--- src/main/java/calculator/domain/Calculator.java
+++ src/main/java/calculator/domain/Calculator.java
@@ -1,21 +1,19 @@
 package calculator.domain;
 
-import java.util.List;
-
 public class Calculator {
     private final Operands operands;
 
-    private Calculator(Operands operands) {
+    public Calculator(Operands operands) {
         this.operands = operands;
     }
 
-    public static Calculator from(String input) {
-        List<String> numbers = Separator.splitByDelimiter(input);
-        Operands operands = Operands.from(numbers);
-        return new Calculator(operands);
+    public int sum() {
+        int result=0;
+        for(int value: operands){
+            result += value;
+        }
+
+        return result;
     }
 
-    public int sum() {
-        return operands.sum();
-    }
 }
--- src/main/java/calculator/domain/Operands.java
+++ src/main/java/calculator/domain/Operands.java
@@ -1,26 +1,19 @@
 package calculator.domain;
 
+import java.util.Iterator;
 import java.util.List;
-import java.util.stream.Collectors;
 
-public class Operands {
-    private List<Operand> operands;
+public class Operands implements Iterable<Integer> {
+    private final List<Operand> operands;
 
-    private Operands(List<Operand> operands) {
+    public Operands(List<Operand> operands) {
         this.operands = operands;
     }
 
-    public static Operands from(List<String> numbers) {
-        List<Operand> operandList = numbers.stream()
-                .map(Operand::from)
-                .collect(Collectors.toList());
-
-        return new Operands(operandList);
-    }
-
-    public int sum() {
+    @Override
+    public Iterator<Integer> iterator() {
         return operands.stream()
-                .mapToInt(Operand::getValue)
-                .sum();
+                .map(Operand::getValue)
+                .iterator();
     }
 }
